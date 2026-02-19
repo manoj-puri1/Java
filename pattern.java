706cc8635496020cbde71eeecb80071133c5c2e1
manoj-puri1 <manojpurist@gmail.com>
2026-02-19 23:50:32 +0545

    pattern

---
+++ pattern.java
@@ -0,0 +1,13 @@
+// Pattern print
+// *
+// * *
+// * * *
+// * * * *
+class pattern {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+    }
+}
