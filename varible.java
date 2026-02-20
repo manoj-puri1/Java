d108776dd7ba8b592ce9eed42a6d096d6c2c0541
manoj-puri1 <manojpurist@gmail.com>
2026-02-20 15:18:07 +0545

    java varibles

---
+++ varible.java
@@ -0,0 +1,11 @@
+public class varible{
+    public static void main(String[] args){
+        // string name = "manoj";
+        int a = 4;
+        int b =5;
+        int sum = a+b;
+        System.out.println(sum);
+    
+
+    }
+}
